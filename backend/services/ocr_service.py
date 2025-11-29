1ce65bb6f95ea37999a4a2aef3417616fc777c53
fuwiak <poczta130@gmail.com>
2025-11-30 02:20:52 +0300

    Исправлен отступ в адаптивной обработке PDF (if-else структура)

--- backend/services/ocr_service.py
+++ backend/services/ocr_service.py
@@ -157,7 +157,7 @@ class OCRService:
                         
                         if page_text:
                             all_text.append(page_text)
-        else:
+                        else:
                             # Fallback: пробуем без preprocessing
                             try:
                                 text = pytesseract.image_to_string(img, lang=tesseract_langs, config='--psm 6 --oem 3')
